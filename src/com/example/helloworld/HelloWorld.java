6d5116ebda0760dd55ac1989f88bdc96765672df
BenBurum <benburum@gmail.com>
2014-09-03 11:48:21 -0400

    Testing helloworld

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Ben on 9/3/2014.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
